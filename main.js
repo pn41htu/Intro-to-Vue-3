3e8719b3604a70238112b68ff4f1faa683755e94
pn41htu <pn41htu@eduvaud.ch>
2025-03-18 15:30:01 +0100

    ex -> 5.5 inclu

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-const product = 'Socks'
+const product = "Socks";
+
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Boots",
+      description: "Lorem Ipsdescription",
+      image: "./assets/images/socks_blue.jpg",
+      url: "https://www.infomaniak.com",
+      inStock: true,
+      inventory: 0,
+      onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: ["S", "M", "L", "XL"],
+    };
+  },
+});
